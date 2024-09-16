74ef1b94179b9c3dab702dea58eb8656b5060094
Morten Kastrup <moka@netcompany.com>
2024-09-16 19:52:20 +0200

    Add project structure and initial files

---
+++ src/data_prep.py
@@ -0,0 +1,13 @@
+# src/data_prep.py
+
+import pandas as pd
+
+def load_and_print_data():
+    data = pd.read_csv('../data/matches.csv', sep=",", header=0, decimal=".")
+
+    data.drop(columns=data.columns[0], axis=1,  inplace=True)
+
+    print(data.head(10).to_string(index=False))
+
+if __name__ == "__main__":
+    load_and_print_data()
---
+++ src/main.py
@@ -0,0 +1,12 @@
+# src/main.py
+
+from data_prep import load_and_print_data
+
+def main():
+    print("Starting the Moka Analytics application...")
+    print("Loading and printing data:")
+    load_and_print_data()
+    print("Data processing complete.")
+
+if __name__ == "__main__":
+    main()
