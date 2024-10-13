f99b3ddbc6cf3e222f2557157a26e63c02326efe
Morten Kastrup <mrevsbeckkastrup@gmail.com>
2024-10-13 21:03:55 +0200

    Add monte carlo simulations

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@
 
 # from data_prep import load_and_print_data
 from Methods.linear_reg import linear_regression_stocks
+from Methods.monte_carlo_simulations import monte_carlo_simulations
 
 def main():
     print("Starting the Moka Analytics application...")
-    print("Running linear regression")
+    print("Running monte carlo simulations")
+    
     #load_and_print_data()
-
-    linear_regression_stocks()
+    #linear_regression_stocks()
+    monte_carlo_simulations()
+    
     print("processing complete.")
 
 if __name__ == "__main__":
