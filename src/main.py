0f78b414a73d953964a3458dd84243fc2c02253f
Morten Kastrup <mrevsbeckkastrup@gmail.com>
2024-10-13 15:00:17 +0200

    Adding linear regression

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 # src/main.py
 
-from data_prep import load_and_print_data
+# from data_prep import load_and_print_data
+from Methods.linear_reg import linear_regression_stocks
 
 def main():
     print("Starting the Moka Analytics application...")
-    print("Loading and printing data:")
-    load_and_print_data()
-    print("Data processing complete.")
+    print("Running linear regression")
+    #load_and_print_data()
+
+    linear_regression_stocks()
+    print("processing complete.")
 
 if __name__ == "__main__":
     main()
